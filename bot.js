52b1e574687c13eed80df9d3c6846f075e25a601
habibullayevhusayn26 <habibullayevhusayn26@gmail.com>
2026-09-19 00:13:37 +0500

    dsdsd

--- bot.js
+++ bot.js
@@ -236,7 +236,7 @@ function buildYoutubeSearchUrl(query, key) {
     q: query,
     key
   });
-  return `https://www.googleapis.com/youtube/v3/search?${params.toString()}`;
+  return `https://www.googleapis.com/youtube/v3/search?${params.toString().replace(/\+/g, '%20')}`;
 }
 
 function buildYoutubeVideosUrl(ids, key) {
